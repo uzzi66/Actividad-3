0cab8bc6279ac9574f2235e4f376eaf562094f81
Uzziel Reyes <123527494+uzzi66@users.noreply.github.com>
2023-02-08 14:03:32 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
+
+Neurona::Neurona(int i,float v,int px,int py,int r,int b,int g){
+    id=i;
+    voltaje=v;
+    posicion_x=px;
+    posicion_y=py;
+    red=r;
+    blue=b;
+    green=g;
+}
+
+void Neurona::print(){
+    cout<<"ID:"<<id<<endl;
+    cout<<"Voltaje:"<<voltaje<<endl;
+    cout<<"Posicion x:"<<posicion_x<<endl;
+    cout<<"Posicion y:"<<posicion_y<<endl;
+    cout<<"Rojo:"<<red<<endl;
+    cout<<"Azul:"<<blue<<endl;
+    cout<<"Verde:"<<green<<endl;
+}
+
+administrar::administrar(){
+    aux=0;
+}
+
+void administrar::agregar_inicio(Neurona n){
+    vect.insert(vect.begin(), n);
+    aux++;
+}
+
+void administrar::agregar_final(Neurona n){
+    vect.insert(vect.end(), n);
+    aux++;
+}
+
+void administrar::mostrar(){
+    for(int i=0;i<aux;i++){
+        vect[i].print();
+    }
+}
